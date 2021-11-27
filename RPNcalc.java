9880590258039ba22e272bc1056ae705f1280457
victor guimaraes <62081666+vfg7@users.noreply.github.com>
2021-11-26 21:34:42 -0300

    Commit-2
    edited rpn calc for quest 2. Ainda preciso desenrolar umas coisas

--- RPNcalc.java
+++ RPNcalc.java
@@ -1,6 +1,13 @@
 import java.util.*;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
 
-public class RPNCalc {
+
+class RPNCalc {
     public Stack<Integer> stack;
 
         public RPNCalc(){
@@ -64,28 +71,174 @@ public class RPNCalc {
 
         return result;
     }
+}
+    
+enum Token{
+    //definindo operações de tokenizáveis bem simples, só as quatro operações básicas e números inteiros
 
-    public static void main (String[]args){
-        
-        String operation, repeat;
-        int result;
+    TK_SUM ("\\+"),
+    TK_SUB ("-"),
+    TK_MUL ("\\*"),
+    TK_DIV ("/"),
+    INTEGER ("\\d+");
+    private final Pattern pattern;
 
-        Scanner in = new Scanner(System.in);
-        RPNCalc calculator = new RPNCalc();
-
-        do{
-
-            calculator.clearStack();
-            System.out.println("ENTER RPN EXPRESSION SUCH AS  '1 2 +'");
-            operation = in.nextLine();
-
-            result = calculator.evaluateExpression(operation);
-            System.out.println(" \n EQUALS: " + result);
-
-            System.out.println("ANOTHER? [Y/N]");
-            repeat = in.nextLine();
-
-        } while (repeat.equalsIgnoreCase("y"));
-
+    Token(String regex) {
+        pattern = Pattern.compile("^" + regex);
     }
+    
+    int endOfMatch(String s){
+        Matcher m = pattern.matcher(s);
+
+        if(m.find()){
+            return m.end();
+        }
+        return -1;
+    }
+
+
+}
+class Lexer {
+    private StringBuilder input = new StringBuilder();
+    private Token token;
+    private String lexema;
+    private boolean exausthed = false;
+    private String errorMessage = "";
+    private Set<Character> blankChars = new HashSet<Character>();
+
+    public Lexer(String expression) {
+        //change file path to added string block
+        // try (Stream<String> st = Files.lines(Paths.get(filePath))) {
+        //     st.forEach(input::append);
+        // } catch (IOException ex) {
+        //     exausthed = true;
+        //     errorMessage = "Could not read file: " + filePath;
+        //     return;
+        // }
+
+
+
+        blankChars.add('\r');
+        blankChars.add('\n');
+        blankChars.add((char) 8);
+        blankChars.add((char) 9);
+        blankChars.add((char) 11);
+        blankChars.add((char) 12);
+        blankChars.add((char) 32);
+
+        moveAhead();
+    }
+
+    public void moveAhead() {
+        if (exausthed) {
+            return;
+        }
+
+        if (input.length() == 0) {
+            exausthed = true;
+            return;
+        }
+
+        ignoreWhiteSpaces();
+
+        if (findNextToken()) {
+            return;
+        }
+
+        exausthed = true;
+
+        if (input.length() > 0) {
+            errorMessage = "Unexpected symbol: '" + input.charAt(0) + "'";
+        }
+    }
+
+    private void ignoreWhiteSpaces() {
+        int charsToDelete = 0;
+
+        while (blankChars.contains(input.charAt(charsToDelete))) {
+            charsToDelete++;
+        }
+
+        if (charsToDelete > 0) {
+            input.delete(0, charsToDelete);
+        }
+    }
+
+    private boolean findNextToken() {
+        for (Token t : Token.values()) {
+            int end = t.endOfMatch(input.toString());
+
+            if (end != -1) {
+                token = t;
+                lexema = input.substring(0, end);
+                input.delete(0, end);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public Token currentToken() {
+        return token;
+    }
+
+    public String currentLexema() {
+        return lexema;
+    }
+
+    public boolean isSuccessful() {
+        return errorMessage.isEmpty();
+    }
+
+    public String errorMessage() {
+        return errorMessage;
+    }
+
+    public boolean isExausthed() {
+        return exausthed;
+    }
+    public static void lexicalAnalyser(Lexer lexer){
+        System.out.println("Lexical Analysis");
+        System.out.println("-----------------");
+        while (!lexer.isExausthed()) {
+            System.out.printf("%-18s :  %s \n",lexer.currentLexema() , lexer.currentToken());
+            lexer.moveAhead();
+        }
+
+        if (lexer.isSuccessful()) {
+            System.out.println("Ok! :D");
+        } else {
+            System.out.println(lexer.errorMessage());
+        }
+    }
+}
+
+
+
+public static void main (String[]args){
+    
+    String operation, repeat;
+    int result;
+
+    Scanner in = new Scanner(System.in);
+    RPNCalc calculator = new RPNCalc();
+
+    do{
+
+        calculator.clearStack();
+        System.out.println("ENTER RPN EXPRESSION SUCH AS  '1 2 +'");
+        
+        operation = in.nextLine();
+        Lexer l = new Lexer(operation);
+        lexicalAnalyser(l);
+
+        result = calculator.evaluateExpression(operation);
+        System.out.println(" \n EQUALS: " + result);
+
+        System.out.println("ANOTHER? [Y/N]");
+        repeat = in.nextLine();
+
+    } while (repeat.equalsIgnoreCase("y"));
+
 }
